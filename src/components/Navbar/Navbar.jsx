72f1d886922de6fd3290b421b312f3650388d8a4
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-20 15:02:35 -0300

    Drag and drop assets folder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={image.logo} alt="logo"></img>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
