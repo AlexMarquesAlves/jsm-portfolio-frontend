bb56672955c3f6a59ef2e357be9f5073b364f9b3
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-20 17:18:57 -0300

    initialize Skills section

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 import "./Skills.scss";
 
